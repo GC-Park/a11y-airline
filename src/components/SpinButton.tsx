bcbeb982c65a12deb246a4d8481ccf8931d01161
Patrick <kuyt3122@naver.com>
2023-07-28 12:26:47 +0900

    refactor: 웹 접근성 잘 나오도록 수정

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,40 +1,51 @@
-import React, { useState, MouseEvent } from "react";
-import "./SpinButton.css";
-
+import React, { useState, MouseEvent, useEffect } from 'react';
+import './SpinButton.css';
 const SpinButton: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
-
+  const [announceText, setAnnounceText] = useState<string>('성인 승객 1명');
   const increment = () => {
-    setCount((prevCount) => prevCount + 1);
+    if (count === 3) {
+      setAnnounceText("3까지만 추가할 수 있습니다.");
+      return};
+    if (count === 0) {
+      setAnnounceText(`성인 ${count + 1} 텍스트 숫자만 수정 `);
+      setCount(prevCount => prevCount + 1);
+      return;
+    }
+    setCount(prevCount => prevCount + 1);
+    setAnnounceText(`성인 승객 추가 ${count + 1}`);
   };
-
   const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
+    if (count === 0) {
+      setAnnounceText("0까지만 감소할 수 있습니다.");
+      return};
+    setCount(prevCount => prevCount - 1);
+    setAnnounceText(`성인 승객 감소 ${count - 1}`);
   };
-
   const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
     setIsTooltipVisible(!isTooltipVisible);
   };
-
+  useEffect(() => {
+    // Announce text updates to screen readers
+    if (announceText) {
+      const liveRegion = document.getElementById('live-region');
+      if (liveRegion) {
+        liveRegion.innerText = announceText;
+      }
+    }
+  }, [announceText]);
   return (
     <section className="spinButtonContainer">
       <div>
         <h1>승객 선택</h1>
         <div className="spinButtonLabel">
           <label>성인</label>
-          <div
-            className="helpIcon"
-            onMouseEnter={toggleTooltip}
-            onMouseLeave={toggleTooltip}
-          >
-            ?
-            {isTooltipVisible && (
-              <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>
-            )}
+          <div className="helpIcon" onMouseEnter={toggleTooltip} onMouseLeave={toggleTooltip}>
+            ?{isTooltipVisible && <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>}
           </div>
         </div>
-        <button onClick={decrement} className="spinButton">
+        <button onClick={decrement} className="spinButton" aria-label="성인 탑승자 한명 줄이기 버튼">
           -
         </button>
         <input
@@ -44,12 +55,12 @@ const SpinButton: React.FC = () => {
           className="spinButtonInput"
           value={count}
         />
-        <button onClick={increment} className="spinButton">
+        <button onClick={increment} className="spinButton" aria-label="성인 탑승자 한명 늘리기 버튼">
           +
         </button>
       </div>
+      <div id="live-region" aria-live="assertive" style={{ position: 'absolute', left: '-9999px' }}></div>
     </section>
   );
 };
-
 export default SpinButton;
